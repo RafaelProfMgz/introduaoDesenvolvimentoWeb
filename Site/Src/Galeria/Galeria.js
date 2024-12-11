04d67033b0500556604873c33ce64beef8c6ee82
rafaelMgz <151933601+rafaelMgz@users.noreply.github.com>
2024-12-10 21:45:35 -0400

    feito na faculdade

--- Site/Src/Galeria/Galeria.js
+++ Site/Src/Galeria/Galeria.js
@@ -8,6 +8,18 @@ document.addEventListener("DOMContentLoaded", () => {
     .catch((error) => {
       console.error("Erro ao carregar a seção NavBar:", error);
     });
+
+  // Carregar Restaurante.html
+  fetch("../../Component/Restaurante/Restaurante.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("restaurante").innerHTML = data;
+      carregarImagens();
+    })
+    .catch((error) => {
+      console.error("Erro ao carregar a seção Restaurante:", error);
+    });
+
   // Carregar Social.html
   fetch("../../Component/Social/Social.html")
     .then((response) => response.text())
@@ -40,5 +52,36 @@ function carregarImagensSocial() {
   }
   if (instagramIcon) {
     instagramIcon.src = socialIcons.instagram;
+  }
+}
+// Função para carregar as imagens e exibir na galeria
+function carregarImagens() {
+  const galeriaContainer = document.getElementById("imagens");
+
+  const totalImagens = 6; 
+
+  // Loop para criar cards de imagens
+  for (let i = 1; i <= totalImagens; i++) {
+    const imageName = `Galeria${i}.jpg`; // Altere a extensão conforme necessário
+
+    // Criar o card de imagem
+    const card = document.createElement("div");
+    card.classList.add("card-imagem");
+
+    const img = document.createElement("img");
+    img.src = `../../Assets/Galeria/${imageName}`;
+    img.alt = `Imagem ${i}`;
+    img.classList.add("imagem-galeria");
+
+    // Adiciona a imagem ao card
+    card.appendChild(img);
+
+    // Adiciona o card à galeria
+    galeriaContainer.appendChild(card);
+
+    // Atraso progressivo para cada imagem
+    setTimeout(() => {
+      card.classList.add("visible");
+    }, i * 300); // Atraso de 300ms entre cada imagem
   }
 }
